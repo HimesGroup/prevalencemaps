ea26588fe2bab7fec7b2834090f76e397892a35b
phllena <47615906+phllena@users.noreply.github.com>
2019-08-01 13:24:24 -0400

    Update ui.R

--- ui.R
+++ ui.R
@@ -21,37 +21,9 @@ body<-dashboardBody(
     tabItem(tabName = "Map",
             fluidRow(
               column(4,
-                     selectInput("control_diseasemap", "Condition of Interest:", 
-                                 choices=c("Asthma" = "Asthma","CHD" = "CHD", "Flushot"= "Flushot"))
-                     ),
-              column(4,
-                     selectInput("varmap", "Year:",
-                                 c("2007" = "2007", "2008" = "2008", "2009" = "2009",
-                                   "2010" = "2010", "2011" = "2011", "2012" = "2012",
-                                   "2013" = "2013", "2014" = "2014", "2015" = "2015",
-                                   "2016" = "2016", "2017" = "2017",
-                                   "2007-2017 (all years)" = "2007-2017 (all years)",
-                                   "2007-2010" = "2007-2010",
-                                   "2011-2017" = "2011-2017"), selected = "2007-2017 (all years)")),
-              column(4,
-                     selectInput("mmsa_inputmap", "MMSA:",
-                                 choices = mmsa_names$x))
-            ),
-            fluidRow(        
-              column(12,
-                     box(width=NULL,
-                         h5(textOutput("mapyearinfo"), align="center"),
-                         leafletOutput("map"),
-                         actionButton("reset_button", "Reset view"),
-                         h5(textOutput("info"), align="center")
-                     ))
-            )),
-    tabItem(tabName = "MMSAGraph",
-            fluidRow(
-              column(4,
                      selectInput("control_disease", "Condition of Interest:", 
                                  choices=c("Asthma" = "Asthma","CHD" = "CHD", "Flushot"= "Flushot"))
-              ),
+                     ),
               column(4,
                      selectInput("var", "Year:",
                                  c("2007" = "2007", "2008" = "2008", "2009" = "2009",
@@ -65,6 +37,16 @@ body<-dashboardBody(
                      selectInput("mmsa_input", "MMSA:",
                                  choices = mmsa_names$x))
             ),
+            fluidRow(        
+              column(12,
+                     box(width=NULL,
+                         h5(textOutput("mapyearinfo"), align="center"),
+                         leafletOutput("map"),
+                         actionButton("reset_button", "Reset view"),
+                         h5(textOutput("mapinfo"), align="center")
+                     ))
+            )),
+    tabItem(tabName = "MMSAGraph",
             fluidRow(
               column(12,
                      h5(textOutput("graphinfo"), align="center"))
