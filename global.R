03a06cfe3ff76efdc3df8cae134b0597429169e8
phllena <47615906+phllena@users.noreply.github.com>
2019-08-01 13:12:02 -0400

    Update global.R

--- global.R
+++ global.R
@@ -59,11 +59,7 @@ colnames(weighted_current_varsflushot) <- c("MMSA", "YEAR", "Flushot", "<$25,000
                                             "Less than high school", "High school", "Some college or more",
                                             "18-24", "25-34", "35-44", "45-54", "55-64", "65+")
 
-current.all<-read_feather("/srv/shiny-server/databases/prevalencemaps/current_all.feather")
-current.all2007_2017<-read_feather("/srv/shiny-server/databases/prevalencemaps/current_all2007_2017.feather")
-current.all2007_2010<-read_feather("/srv/shiny-server/databases/prevalencemaps/current_all2007_2010.feather")
-current.all2011_2017<-read_feather("/srv/shiny-server/databases/prevalencemaps/current_all2011_2017.feather")
-
-des2007_2017<-readRDS("/srv/shiny-server/databases/prevalencemaps/des2007_2017.rds")
-des2007_2010<-readRDS("/srv/shiny-server/databases/prevalencemaps/des2007_2010.rds")
-des2011_2017<-readRDS("/srv/shiny-server/databases/prevalencemaps/des2011_2017.rds")
+current.all<-read_feather("/srv/shiny-server/databases/prevalencemaps/current_all.RDS")
+current.all2007_2017<-read_feather("/srv/shiny-server/databases/prevalencemaps/current_all2007_2017.RDS")
+current.all2007_2010<-read_feather("/srv/shiny-server/databases/prevalencemaps/current_all2007_2010.RDS")
+current.all2011_2017<-read_feather("/srv/shiny-server/databases/prevalencemaps/current_all2011_2017.RDS")
